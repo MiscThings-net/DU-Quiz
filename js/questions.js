f59c4fde05bf13d3ec558e25beaa956429028afe
seungwook0417 <lee@seungwook.me>
2022-06-06 00:23:22 +0900

    Initial commit

---
+++ js/questions.js
@@ -0,0 +1,123 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "대구대학교 공식 캐릭터 이름은?",
+    answer: "두두",
+    options: [
+      "뽀로로",
+      "다다",
+      "두두",
+      "도라에몽"
+    ]
+  },
+    {
+    numb: 2,
+    question: "대구대학교 본관은 어디에 있나요?",
+    answer: "경산시",
+    options: [
+      "부산광역시",
+      "대구광역시",
+      "경산시",
+      "서울특별시"
+    ]
+  },
+    {
+    numb: 3,
+    question: "대구대학교 학과중 아닌것은?",
+    answer: "KFC학과",
+    options: [
+      "특수교육과",
+      "언어치료학과",
+      "직업재활학과",
+      "KFC학과"
+    ]
+  },
+    {
+    numb: 4,
+    question: "대구대학교 창립일은 언제 인가요?",
+    answer: "1956년 5월 1일",
+    options: [
+      "2002년 2월 4일",
+      "1997년 4월 17일",
+      "1950년 6월 25일",
+      "1956년 5월 1일"
+    ]
+  },
+    {
+    numb: 5,
+    question: "대구대학교 크기는 전국에서 몇번째 인가요?",
+    answer: "3번째",
+    options: [
+      "1번째",
+      "2번째",
+      "3번째",
+      "4번째"
+    ]
+  },
+  {
+  numb: 6,
+  question: "대구대학교의 기숙사가 아닌것은?",
+  answer: "드림기숙사",
+  options: [
+    "비호생활관",
+    "향토생활관",
+    "행복기숙사",
+    "드림기숙사"
+  ]
+},{
+  numb: 7,
+  question: "다움중 대구대학교의 도서관은?",
+  answer: "창파도서관",
+  options: [
+    "문파도서관",
+    "창솔도서관",
+    "창파도서관",
+    "창미도서관"
+  ]
+},{
+  numb: 8,
+  question: "다움 중 본교의 교화로 알맞은 것은?",
+  answer: "백일홍",
+  options: [
+    "개나리",
+    "백일홍",
+    "장미",
+    "진달래"
+  ]
+},{
+  numb: 9,
+  question: "다움 중 본교에 없는 시설은?",
+  answer: "볼링장",
+  options: [
+    "골프장",
+    "수영장",
+    "볼링장",
+    "헬스장"
+  ]
+},{
+  numb: 10,
+  question: "다움 중 본교에 없는 광장은?",
+  answer: "비호광장",
+  options: [
+    "사랑광장",
+    "비호광장",
+    "자유광장",
+    "빛광장"
+  ]
+},
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
